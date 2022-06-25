eb8c7e4095f435aeeb3ab118f42f211c02dea134
mfahad.itp <mfahad.itp@sparkcognition.com>
2022-06-25 15:42:15 +0500

    complete project

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,12 @@ from requests.structures import CaseInsensitiveDict
 import requests
 import pytest
 from config import BASE_URL, TOKEN_BASE_URL
-global TOKE
 
 
 @pytest.fixture(scope='class')
 def token():
-    payload = {'type': 'abc', 'certain_id': 'xyz', 'user': 'test_user',
-            'password': 'Test@123'}
+    payload = {'id': 'xyz', 'user': 'test_user',
+            'pass': 'Test@123'}
     response = requests.post(f'{TOKEN_BASE_URL}/a/b/c/d/e-f-g/h', data=payload)
     return "Bearer " + response.json()['auth_token']
 
